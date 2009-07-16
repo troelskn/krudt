d8114f369a6c6066ec51627144a844c98728dd94
Troels <tkn@tkn-laptop.(none)>
2009-07-17 01:53:00 +0200

    Finetuning distribution

---
+++ install.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+if [ $# != 1 ]
+then
+  echo "USAGE: $0 NAME"
+  exit 255
+fi
+
+echo $1 | grep -Eq '^[a-z0-9_.-]*$'
+if [ $? != 0 ]
+then
+  echo "Illegal name $1"
+  exit 255
+fi
+
+if [ -e $1 ]
+then
+  echo "$1 already exists"
+  exit 255
+fi
+
+set -v
+# Pull Konstrukt starterpack
+svn export http://konstrukt.googlecode.com/svn/trunk/examples/starterpack_default $1
+cd $1
+# Pull krudt addon
+cp -rf /home/tkn/public/krudt/* .
+# Pull git://github.com/troelskn/bucket.git
+mkdir -p thirdparty/bucket
+wget -q -O - http://github.com/troelskn/bucket/tarball/master | tar -xz -C thirdparty/bucket
+# Pull https://pdoext.googlecode.com/svn/trunk
+svn export https://pdoext.googlecode.com/svn/trunk thirdparty/pdoext
+# Set file permissions
+chmod 777 log
+chmod 777 var
+cd ..
+set +v
