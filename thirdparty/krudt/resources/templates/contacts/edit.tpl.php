3db37ca7b1c76d700b7c6e61255a2cd3673a5858
Lars Olesen <lars@intraface.dk>
2010-03-10 06:12:11 +0100

    Bugfix. Removed this.

--- thirdparty/krudt/resources/templates/contacts/edit.tpl.php
+++ thirdparty/krudt/resources/templates/contacts/edit.tpl.php
@@ -1,5 +1,5 @@
 <h2>Edit <?php e($contact->display_name()); ?></h2>
-<?php print $viewhelper->this->html_form_tag('put', url('', array('edit'))); ?>
+<?php print $viewhelper->html_form_tag('put', url('', array('edit'))); ?>
 <?php print $viewhelper->errors($contact); ?>
 <?php include('form.tpl.php'); ?>
 </form>
