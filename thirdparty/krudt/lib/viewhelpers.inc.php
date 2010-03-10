5db6cf531ad68c7794f358e6d1dce227b327dcd2
Lars Olesen <lars@intraface.dk>
2010-03-10 11:58:38 +0100

    Removed e() again as it is outputting directly. Replaced with htmlentities.

--- thirdparty/krudt/lib/viewhelpers.inc.php
+++ thirdparty/krudt/lib/viewhelpers.inc.php
@@ -6,7 +6,7 @@
     $method = strtolower($method);
     $action = $action ? $action : url();
     $html = "";
-    $html .= '<form method="' . e($method === 'get' ? 'get' : 'post') . '" action="' . e($action) . '">';
+    $html .= '<form method="' . htmlentities($method === 'get' ? 'get' : 'post') . '" action="' . htmlentities($action) . '">';
     if ($method !== 'get' && $method !== 'post') {
       $html .= '<input type="hidden" name="_method" value="' . $method . '" />';
     }
@@ -28,9 +28,9 @@
     $href_back = $href_back ? $href_back : url();
     $html = "";
     $html .= "\n" . '<p class="form-footer">';
-    $html .= "\n" . '<a href="' . e($href_back) . '">Cancel</a>';
+    $html .= "\n" . '<a href="' . htmlentities($href_back) . '">Cancel</a>';
     $html .= "\n" . ':';
-    $html .= "\n" . '<input type="submit" value="' . e($submit_title) . '" />';
+    $html .= "\n" . '<input type="submit" value="' . htmlentities($submit_title) . '" />';
     $html .= "\n" . '</p>';
     return $html;
   }
@@ -42,7 +42,7 @@
     $html = "";
     foreach ($entity->errors as $field => $error) {
       if (!is_string($field)) {
-        $html .= "\n" . '<p style="color:red">' . e($error) . '</p>';
+        $html .= "\n" . '<p style="color:red">' . htmlentities($error) . '</p>';
       }
     }
     return $html;
@@ -54,11 +54,11 @@
   function krudt_html_text_field($entry, $field, $label = null) {
     $label || $label = ucfirst(str_replace('_', ' ', $field));
     $html = '  <p class="krudt-form">
-    <label for="field-' . e($field) . '">' . e($label) . '</label>
-    <input type="text" id="field-' . e($field) . '" name="' . e($field) . '" value="' . e($entry->{$field}()) . '" />
+    <label for="field-' . htmlentities($field) . '">' . htmlentities($label) . '</label>
+    <input type="text" id="field-' . htmlentities($field) . '" name="' . htmlentities($field) . '" value="' . htmlentities($entry->{$field}()) . '" />
 ';
     if (isset($entry->errors[$field])) {
-      $html .= '    <span style="display:block;color:red">' . e($entry->errors[$field]) . ' </span>
+      $html .= '    <span style="display:block;color:red">' . htmlentities($entry->errors[$field]) . ' </span>
 ';
     }
     $html .= "  </p>\n";
@@ -69,12 +69,12 @@
    * Creates a `<table>` containing a collection.
    */
   function krudt_collection($context, $collection, $slug, $fields = null, $row_actions = null, $collection_actions = null) {
-    return new krudt_view_CollectionWidget($context, $collection, $slug);
+    return new krudt_view_CollectionWidget($collection, $context, $slug);
   }
 
   /**
    * Creates a pagination widget for a collection.
    */
   function krudt_paginate($context, $collection, $size = 10) {
-    return new krudt_view_SimplePaginateWidget($context, $collection, $size);
+    return new krudt_view_SimplePaginateWidget($collection, $size, $context);
   }
